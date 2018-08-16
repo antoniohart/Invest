7c2cf6d9159ca58a46bce4a95e5ca6235f6bd3f0
antoniohart <ntow89@gmail.com>
2018-08-16 14:51:25 +0000

    Populating with data

--- app/src/main/java/com/dummies/invest/MainActivity.java
+++ app/src/main/java/com/dummies/invest/MainActivity.java
@@ -1,40 +1,20 @@
 package com.dummies.invest;
 
-import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.TextView;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.kosalgeek.genasync12.AsyncResponse;
 import com.kosalgeek.genasync12.PostResponseAsyncTask;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.HashMap;
-import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -43,6 +23,18 @@ public class MainActivity extends AppCompatActivity {
     private String name;
     private Double deposit;
     private Double rate;
+    private TextView  textView1;
+    private TextView  textView2;
+    private TextView  textView3;
+    private TextView  textView4;
+   // private Double deposit;
+    private String  bankID;
+    private String  bankName;
+    private String  invType;
+    private String  rates; //private Double rate;
+    private String  period;
+    private String  invId;
+
 
     public static final int CONNECTION_TIMEOUT=10000;
     public static final int READ_TIMEOUT=15000;
@@ -62,7 +54,12 @@ public class MainActivity extends AppCompatActivity {
            Search =findViewById(R.id.search);
            amount =findViewById(R.id.amount);
 
+
+
            HighRateContainer = findViewById(R.id.card2);
+        textView1   = findViewById(R.id.textView3);
+        textView2   = findViewById(R.id.textView4);
+        textView3   = findViewById(R.id.textView5);
 
 
 
@@ -87,7 +84,26 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void processFinish(String apiData) {
 
-                    Toast.makeText(MainActivity.this, apiData.toString(), Toast.LENGTH_SHORT).show();
+              //      Toast.makeText(MainActivity.this, apiData.toString(), Toast.LENGTH_SHORT).show();
+
+
+                    String [] info  =apiData.split("/");
+                    bankID=info[0]; bankName=info[1]; invType=info[2]; rates= info[3]; period=info[4]; invId=info[5];
+                    //for bankname
+                    textView1.setText( bankName);
+
+                    //for invType
+                    textView2.setText( invType);
+
+
+
+                    //for rates
+
+                    rate=  100* Double.parseDouble(rates);
+                    //  StringBuffer sb = new StringBuffer(rate);
+                    textView3.setText( String.valueOf( rate )+ "%");
+
+
 
                 }
             });
